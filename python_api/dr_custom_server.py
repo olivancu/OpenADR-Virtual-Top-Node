fe747549f3b06e6a93377bd6a5c34651fe3a888d
Olivancu <vancutsemolivier@gmail.com>
2019-04-30 15:32:39 -0700

    Server now offers GET interface, instead of pushing events

--- python_api/dr_custom_server.py
+++ python_api/dr_custom_server.py
@@ -1,12 +1,15 @@
-__author__ = 'Olivier Van Cutsem'
-
-import time, json
+# !flask/bin/python
+from flask import Flask, request, g
+import json
+import time
 from threading import Timer
 import logging
 from os import listdir
 from os.path import isfile, join
+import threading
 from drevent_manager import DReventManager, read_from_json
-import requests
+
+app = Flask(__name__)
 
 ### LOGGER
 FORMAT = '%(asctime)-15s %(message)s'
@@ -18,98 +21,139 @@ logger.setLevel(logging.INFO)
 TIME_REFRESH_EVENTS = 10  # The sleeping time, between which the DR events are updated from the files
 FOLDER_DR_EVENTS = './dr-custom-data/'
 EXTERNAL_API = "http://127.0.0.1:5000"
-MAPPING_DREVENT_API = {'dr_prices': 'add-dr-signal/price',
-                       'dr_shed': 'add-dr-signal/shed',
-                       'dr_limit': 'add-dr-signal/limit',
-                       'dr_shift': 'add-dr-signal/shift',
-                       'dr_track': 'add-dr-signal/track'}
+
+list_dr_events = ['dr_prices','dr_shed', 'dr_limit', 'dr_shift', 'dr_track']
 
 ### SERVER VARIABLES
 
-# THE GLOBAL VARIABLE THAT KEEPS THE INFINITE LOOP RUNNING
-KEEP_SERVER_ON = True
+KEEP_READING_FILE = True
 
 # DR events information and state, to keep track of change and trigger new ones
-drevent_manager = DReventManager()
+dr_manager = DReventManager()
 
-def stop_server():
-    global KEEP_SERVER_ON
-    KEEP_SERVER_ON = False
+def get_drevent_manager():
+    return dr_manager
+
+#
+#### --- Flask API --- ####
+#
+
+def print_receive_data(data_type, data_payload):
+    print "Receive {} signal from the DR server:".format(data_type)
+    print data_payload
+
+# - Test
+@app.route('/')
+def api_root():
+    return 'Welcome to the Solar+ DR Server'
+
+
+@app.route('/get-all-signal', methods=['GET'])
+def get_all_signal():
+    return get_dr_signal(None)
+
+@app.route('/get-dr-signal/<type_dr>', methods=['GET'])
+def get_dr_signal(type_dr):
+
+    st = request.args.get('startdate')
+    et = request.args.get('enddate')
+
+    timeframe = None
+    if (st is not None) and (et is not None):
+        timeframe = (st, et)
+
+    ev_manager = get_drevent_manager()
+    list_events = ev_manager.get_available_events(type_dr, timeframe)
+
+    return json.dumps(list_events)
+
+#
+#### --- File management to update the DR events available --- ####
+#
 
 def init_event_scheduler():
     """
     Read the type of DR events from the file names
     :return:
     """
-    global drevent_manager
+    global dr_manager
 
     for f in listdir(FOLDER_DR_EVENTS):
         if isfile(join(FOLDER_DR_EVENTS, f)):
             name_dr = f.split(".")[0]
-            drevent_manager.set_scheduled_amount(name_dr, 0)
+            dr_manager.set_scheduled_amount(name_dr, 0)
 
-def push_event_to_api(type_event, dr_data):
+def push_event_to_queue(dr_data):
 
-    r = requests.post('{}/{}'.format(EXTERNAL_API, MAPPING_DREVENT_API[type_event]), json=dr_data)
+    global dr_manager
 
-    if r.status_code == 200:
-        logger.info("New event of type {0} sent to the external API".format(type_event))
-    else:
-        logger.warning("Error while sending an event of type {0} to the external API".format(type_event))
+    dr_manager.add_available_event(dr_data)
 
 def add_dr_event(type_dr, dr_raw_data):
 
     # Decode the event and create the timeseries
-    scheduled_time, data_dr = drevent_manager.decode_rawjson(type_dr, dr_raw_data)
-    delay = max(0, scheduled_time - time.time())
+    notif_time, data_dr = dr_manager.decode_rawjson(type_dr, dr_raw_data)
+
+    delay = max(0, notif_time - time.time())
 
     # Scheduling the event to be triggered later
-    logger.info("Adding an event of type {0} to be scheduled at time {1} ({2} seconds from now)".format(type_dr, scheduled_time, delay))
-    Timer(delay, push_event_to_api, (type_dr, data_dr)).start()
+    logger.info("Adding an event of type {0} to be available at time {1} ({2} seconds from now)".format(type_dr, notif_time, delay))
+    Timer(delay, push_event_to_queue, (data_dr,)).start()
 
 def update_dr_events():
     """
     Read the files describing the DR events and add them internally
     :return:
     """
-    global drevent_manager
+    global dr_manager
 
     for f in listdir(FOLDER_DR_EVENTS):
         filepath = join(FOLDER_DR_EVENTS, f)
         if isfile(join(FOLDER_DR_EVENTS, f)):
+            extension_file = f.split(".")[1]
             type_dr = f.split(".")[0]
 
-            # Get the list of event
-            dr_list = read_from_json(filepath)
-            if dr_list is None: continue
-            if type(dr_list) is not list:
-                logger.warning("Could not read events in {} because this is not a list".format(f))
-                continue
+            if extension_file == 'json':
 
-            # Compare the last stored length with the current file list length
-            if len(dr_list) > drevent_manager.get_scheduled_amount(type_dr):
-                for i in range(len(dr_list) - drevent_manager.get_scheduled_amount(type_dr)):
-                    add_dr_event(type_dr, dr_list[i])
-                    drevent_manager.set_scheduled_amount(type_dr, len(dr_list))
+                # Get the list of event
+                dr_list = read_from_json(filepath)
+                if dr_list is None: continue
+                if type(dr_list) is not list:
+                    logger.warning("Could not read events in {} because this is not a list".format(f))
+                    continue
 
+                # Compare the last stored length with the current file list length
+                if len(dr_list) > dr_manager.get_scheduled_amount(type_dr):
+                    for i in range(len(dr_list) - dr_manager.get_scheduled_amount(type_dr)):
+                        add_dr_event(type_dr, dr_list[i])
+                    dr_manager.set_scheduled_amount(type_dr, len(dr_list))
 
-if __name__ == '__main__':
+#### --- FIlE READER PROCESS --- ####
 
-    logger.info("#")
-    logger.info("### RUNNING THE CUSTOM DR SERVER ###")
-    logger.info("#")
+def file_reading_loop():
+    global KEEP_READING_FILE
 
     # Init the scheduler state to keep track of the updates
     init_event_scheduler()
 
     # Run the server infinite loop
-    while KEEP_SERVER_ON:
-
+    while KEEP_READING_FILE:
         # Read the events list, check if there is new ones and add them
         logger.info(" Updating the event list")
         update_dr_events()
 
         # Sleep
         time.sleep(TIME_REFRESH_EVENTS)
+
+
+if __name__ == '__main__':
+    logger.info("#")
+    logger.info("### RUNNING THE CUSTOM DR SERVER ###")
+    logger.info("#")
+
+    t = threading.Thread(target=file_reading_loop)
+    t.start()
+    app.run(debug=True, use_reloader=False)
+    t.join()
 
     logger.info("--- KILLING THE CUSTOM DR SERVER ---")
--- python_api/drevent_manager.py
+++ python_api/drevent_manager.py
@@ -5,6 +5,9 @@ from datetime import datetime
 import json
 import os, sys
 from dateutil.parser import parse
+import pandas as pd
+
+DEFAULT_DT = '1H'
 
 def read_from_json(filename):
     """
@@ -35,10 +38,12 @@ from cost_calculator.cost_calculator import CostCalculator
 from cost_calculator.tariff_structure import *
 from openei_tariff.openei_tariff_analyzer import *
 
+
 class DReventManager():
 
     def __init__(self):
-        self.__events_state = {}
+        self.__events_state = {}  # The amount of scheduled events since the beginning
+        self.__scheduled_queue = []  # This is an ordered list
 
         self.__tariff_manager = CostCalculator()
 
@@ -51,24 +56,72 @@ class DReventManager():
     def set_scheduled_amount(self, type_dr, v):
         self.__events_state[type_dr] = v
 
+    def add_available_event(self, ev):
+
+        # TODO: insert the ev at the right place in the list
+        self.__scheduled_queue.append(ev)
+
+    def get_available_events(self, type_dr=None, timeframe= None):
+
+        ret_sorted = self.__scheduled_queue
+
+        # No specification: all the data
+        if type_dr is None and timeframe is None:
+            return [{'type': ev['type_dr'], 'data': ev['data_dr'].to_json()} for ev in ret_sorted if ev['data_dr'] is not None]
+
+        # Data type is specified
+        if type_dr is not None:
+            ret_sorted = [ev for ev in ret_sorted if ev['type_dr'] == type_dr]
+
+        if timeframe is None:
+            return [ev['data_dr'].to_json() for ev in ret_sorted]
+
+        # Both timeframe and type of data are speficied
+        # (1) get the signals overlapping with this timeframe (2) shorten the signal
+        st, et = timeframe
+        ret_sorted = [x for x in ret_sorted if (x['startdate'] is not None) and (x['enddate'] is not None)]
+        ret_sorted = [x for x in ret_sorted if (st <= x['startdate'] <= et) or (st <= x['enddate'] <= et) or (x['startdate'] <= st and x['enddate'] >= et)]
+        ret_sorted = [ev['data_dr'][(ev['data_dr'].index >= st) & (ev['data_dr'].index <= et)].to_json() for ev in ret_sorted]
+
+        return ret_sorted
+
     def decode_rawjson(self, type_dr, raw_data):
         notif_time = time.mktime(datetime.strptime(raw_data['notification-date'], "%Y-%m-%dT%H:%M:%S").timetuple())
-        data_dr = None
+        ret_dict = {'type_dr': type_dr, 'startdate': None, 'enddate': None, 'data_dr': None}
 
         if type_dr == 'dr_prices':
-            data_dr = self.get_timeseries_tariff(raw_data["type"], (raw_data["start-date"], raw_data["end-date"]), raw_data["data"])
-        elif type_dr == 'dr_shed':
-            pass
-        elif type_dr == 'dr_limit':
-            pass
+            ret_dict['startdate'] = raw_data["start-date"]
+            ret_dict['enddate'] = raw_data["end-date"]
+            ret_dict['data_dr'] = self.get_df_tariff(raw_data["type"], (raw_data["start-date"], raw_data["end-date"]), raw_data["data"])
+        elif type_dr == 'dr_shed' or type_dr == 'dr_limit':
+            raw_data = raw_data["data"]
+            ret_dict['startdate'] = raw_data["start-date"]
+            ret_dict['enddate'] = raw_data["end-date"]
+            ret_dict['data_dr'] = self.get_df_powerconstraints([(raw_data["start-date"], raw_data["end-date"])], [raw_data["power"]])
         elif type_dr == 'dr_shift':
-            pass
+            raw_data = raw_data["data"]
+            ret_dict['startdate'] = raw_data["start-date-take"]
+            ret_dict['enddate'] = raw_data["end-date-relax"]
+            timeframe_list = [(raw_data["start-date-take"], raw_data["end-date-take"]), (raw_data["start-date-relax"], raw_data["end-date-relax"])]
+            power_list = [raw_data["power-take"], raw_data["power-relax"]]
+            ret_dict['data_dr'] = self.get_df_powerconstraints(timeframe_list, power_list)
         elif type_dr == 'dr_track':
-            pass
+            raw_data = raw_data["data"]
+            ret_dict['startdate'] = raw_data["start-date"]
+            ret_dict['enddate'] = raw_data["end-date"]
+            ret_dict['data_dr'] = self.get_df_powersignal((raw_data["start-date"], raw_data["end-date"]), raw_data['profile'])
+            print ret_dict
 
-        return notif_time, data_dr
+        return notif_time, ret_dict
 
-    def get_timeseries_tariff(self, type_tariff, date_period, raw_json_data):
+    def get_df_tariff(self, type_tariff, date_period, raw_json_data):
+        """
+        Return a Pandas dataframe of elec prices
+        :param type_tariff: 'price-rtp' or 'price-tou'
+        :param date_period: (start_date, end_date )
+        :param raw_json_data: the name of the json file
+        :return: a pandas dataframe
+        """
         start_date, end_date = date_period
 
         if type_tariff == 'price-tou':
@@ -84,9 +137,46 @@ class DReventManager():
             end_date_sig = parse(end_date)
             price_df, map = self.__tariff_manager.get_electricity_price((start_date_sig, end_date_sig), timestep)
 
-            return price_df.to_json()
+            return price_df
 
         elif type_tariff == 'price-rtp':
-            return {'timestamp': [], 'energy_price': raw_json_data}
+            st, et = date_period
+            index = pd.date_range(st, et, freq=DEFAULT_DT)
+            return pd.DataFrame(data=raw_json_data, index=index)
         else:
-            return {}
+            return None
+
+    def get_df_powerconstraints(self, l_timeframe, l_power_constraint):
+        """
+        Create and fill a Pandas dataframe with Power information between multiple timeframe
+        :param timeframe: a list of tuple
+        :param power_constraint: a list of power data
+        :return:
+        """
+
+        df = None
+        for timeframe, power_constraint in zip(l_timeframe, l_power_constraint):
+            st, et = timeframe
+            index = pd.date_range(st, et, freq=DEFAULT_DT)
+
+            new_df = pd.DataFrame(data=len(index)* [power_constraint], index=index, columns=['power'])
+
+            if df is None:
+                df = new_df
+            else:
+                df.append(new_df)
+
+        return df
+
+    def get_df_powersignal(self, timeframe, signal):
+        """
+        Create and fill a Pandas dataframe with Power information between multiple timeframe
+        :param timeframe: a list of tuple
+        :param power_constraint: a list of power data
+        :return:
+        """
+        st, et = timeframe
+        index = pd.date_range(st, et, freq=DEFAULT_DT)
+        df = pd.DataFrame(data=signal, index=index, columns=['power'])
+
+        return df
